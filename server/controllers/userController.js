3699e5c53e9857f5062f65e688044bb8cbbef6b2
Mudassir742 <mudassirwaheed242@gmail.com>
2022-02-18 17:48:17 +0500

    generate jwt token

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.userLogin = async (req, res) => {
   try {
@@ -8,16 +10,67 @@ exports.userLogin = async (req, res) => {
       return res.status(422).json({ error: "fields are empty", data: null });
     }
 
-    const existedUser = await User.find(
-      { email: email, password: password },
-      { _id: 1, name: 1 }
-    );
+    const existedUser = await User.findOne({ email: email });
 
-    if (existedUser.length === 0) {
+    console.log(existedUser);
+
+    if (!existedUser) {
       return res.status(422).json({ error: "user not found", data: null });
     }
 
-    return res.status(201).json({ error: null, data: existedUser[0] });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existedUser.password
+    );
+
+    console.log(isPasswordCorrect);
+
+    if (!isPasswordCorrect) {
+      return res
+        .status(422)
+        .json({ error: "incorrect email or password", data: null });
+    }
+
+    jwt.sign(
+      {
+        name: existedUser.name,
+        userId: existedUser._id,
+        password: existedUser.password,
+      },
+      process.env.JWT_KEY,
+      { expiresIn: "1h" },
+      async (err, token) => {
+        //if (err) throw err.message;
+        if (err) {
+          console.log(err.message);
+          return res
+            .status(422)
+            .json({ error: "unable to generate token", data: null });
+        }
+
+        console.log(token);
+
+        const saveToken = await User.updateOne(
+          { _id: existedUser._id },
+          { $set: { token: token } }
+        );
+
+        if (saveToken) {
+          return res.status(201).json({
+            error: null,
+            data: {
+              name: existedUser.name,
+              userID: existedUser._id,
+              token: token,
+            },
+          });
+        }
+
+        return res
+          .status(422)
+          .json({ error: "unable to save token", data: null });
+      }
+    );
   } catch (err) {
     console.log("Error in Login catch : ", err.message);
     return res.status(422).json({ error: err.message, data: null });
@@ -39,16 +92,21 @@ exports.userSignup = async (req, res) => {
         .json({ error: "email already exists", data: null });
     }
 
+    const encryptedPasswrod = await bcrypt.hash(password, 10);
+
     const newUser = new User({
       name: name,
       email: email,
-      password: password,
+      password: encryptedPasswrod,
     });
 
     const registerUser = await newUser.save();
 
     if (registerUser) {
-      return res.status(201).json({ error: null, data: registerUser });
+      return res.status(201).json({
+        error: null,
+        data: { userID: registerUser._id, name: registerUser.name },
+      });
     }
 
     return res
