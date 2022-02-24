be3a755dd629846ef0b840dfaea3a45352188415
Mudassir742 <mudassirwaheed242@gmail.com>
2022-02-24 18:56:34 +0500

    add function prototype in book controller

---
+++ server/controllers/bookController.js
@@ -0,0 +1,21 @@
+const Books =  require("../models/bookModel")
+
+exports.addNewBook = (req,res) =>{
+
+}
+
+exports.deleteBook = (req,res) =>{
+
+}
+
+exports.showBooks = (req,res) =>{
+
+}
+
+exports.showBookbyId = (req,res) =>{
+
+}
+
+exports.updateBookInfo = (req,res) =>{
+
+}
