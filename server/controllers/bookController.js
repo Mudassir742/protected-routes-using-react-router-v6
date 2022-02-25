99776572b795acfea05c74af5b0ebeb57d485ace
Mudassir742 <mudassirwaheed242@gmail.com>
2022-02-25 15:39:51 +0500

    delete a book

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -45,8 +45,32 @@ exports.addNewBook = async (req, res) => {
   }
 };
 
-exports.deleteBook = (req, res) => {
-    
+exports.deleteBook = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+
+    if (!bookId) {
+      return res.status(422).json({ error: "book id not given", data: null });
+    }
+
+    const isBookDeleted = await Books.deleteOne({ _id: bookId });
+
+    console.log(isBookDeleted.deletedCount);
+
+    if (!isBookDeleted.deletedCount) {
+      return res
+        .status(422)
+        .json({ error: "error while deleting book", data: null });
+    }
+
+    return res.status(201).json({ error: null, data: isBookDeleted });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(422).json({
+      error: "unexpected error occurred while deleting book",
+      data: null,
+    });
+  }
 };
 
 exports.showBooks = (req, res) => {};
--- server/routes/bookRoute.js
+++ server/routes/bookRoute.js
@@ -1,9 +1,20 @@
-const router = require("express").Router()
+const router = require("express").Router();
 
-const bookController = require("../controllers/bookController")
-const verifyToken = require("../middleware/authMiddleware")
-const {verifyRole} = require("../middleware/restrictToMiddleware")
+const bookController = require("../controllers/bookController");
+const verifyToken = require("../middleware/authMiddleware");
+const { verifyRole } = require("../middleware/restrictToMiddleware");
 
-router.post('/addbook',verifyToken,verifyRole(["manager"]),bookController.addNewBook)
+router.post(
+  "/addbook",
+  verifyToken,
+  verifyRole(["manager"]),
+  bookController.addNewBook
+);
+router.delete(
+  "/deletebook/:id",
+  verifyToken,
+  verifyRole(["manager"]),
+  bookController.deleteBook
+);
 
-module.exports = router
+module.exports = router;
